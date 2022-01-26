c368b49b78b4b3f83c973a58ff70ecc3d5c6e639
Dave Foley <davidmfoley@gmail.com>
2022-01-25 19:29:06 -0800

    Use template literal type for score

--- src/getScore.ts
+++ src/getScore.ts
@@ -1,16 +1,21 @@
 import { stdin as input, stdout as output } from 'process'
 import readline from 'readline'
 
+type LetterScore = '.' | 'G' | 'Y'
+type WordScore =
+  | 'X'
+  | `${LetterScore}${LetterScore}${LetterScore}${LetterScore}${LetterScore}`
 const rl = readline.createInterface({ input, output })
 
 const getInputFromStdio = (): Promise<string> =>
   new Promise((resolve) => rl.question('How did I do?\n', resolve))
 
-export const validScore = (score: string) => /^([.YG]{5}|X)$/.test(score)
+export const validScore = (score: string): score is WordScore =>
+  /^([.YG]{5}|X)$/.test(score)
 
 export const getScore = async (
   inputScore = getInputFromStdio
-): Promise<string> => {
+): Promise<WordScore> => {
   while (true) {
     const score = (await inputScore()).toUpperCase()
 

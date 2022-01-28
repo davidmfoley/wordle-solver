0ba93f7a31d168a923cbf9a05b39c19ce1a83773
Dave Foley <davidmfoley@gmail.com>
2022-01-28 13:18:41 -0800

    Predicatify guessFilter

--- src/guessFilter.ts
+++ src/guessFilter.ts
@@ -1,32 +1,48 @@
-export const guessFilter = (guess: string, score: string) => {
-  const scores = score.split('')
+type WordPredicate = (word: string) => boolean
 
-  const letters = guess
+type GetPredicate = (letter: string, index: number) => WordPredicate
+
+interface GuessLetter {
+  score: string
+  letter: string
+  index: number
+}
+
+const lettersWithScores = (guess: string, score: string): GuessLetter[] =>
+  guess
     .split('')
-    .map((letter, index) => ({ letter, index, score: scores[index] }))
+    .map((letter, index) => ({ letter, index, score: score[index] }))
 
-  const yellow = letters.filter(({ score }) => score === 'Y')
-  const green = letters.filter(({ score }) => score === 'G')
+const predicates: Record<string, GetPredicate> = {
+  '.': (letter, index) => (word) => word[index] !== letter,
+  G: (letter, index) => (word) => word[index] === letter,
+  Y: (letter, index) => (word) =>
+    word[index] !== letter && word.includes(letter),
+}
+
+const getLetterPredicate = ({ score, letter, index }: GuessLetter) => {
+  const predicate = predicates[score]
+  if (!predicate) throw new Error('invalid score')
+  return predicate(letter, index)
+}
+
+const unusedPredicate = (letters: GuessLetter[]) => {
   const dot = letters.filter(({ score }) => score === '.')
-
-  const allUsed = [...yellow, ...green]
-
+  const allUsed = letters.filter(({ score }) => score !== '.')
   const unused = dot.filter(
     (l) => !allUsed.some(({ letter }) => letter === l.letter)
   )
 
-  return (word: string) => {
-    if (dot.some(({ letter, index }) => word[index] === letter)) return false
-    if (unused.some(({ letter }) => word.includes(letter))) return false
-    if (green.some(({ letter, index }) => word[index] !== letter)) return false
+  return (word: string) => !unused.some(({ letter }) => word.includes(letter))
+}
 
-    if (
-      yellow.some(
-        ({ letter, index }) => word[index] === letter || !word.includes(letter)
-      )
-    )
-      return false
+export const guessFilter = (guess: string, score: string) => {
+  const letters = lettersWithScores(guess, score)
 
-    return true
-  }
+  const predicates = [
+    ...letters.map(getLetterPredicate),
+    unusedPredicate(letters),
+  ]
+
+  return (word: string) => !predicates.some((p) => !p(word))
 }
